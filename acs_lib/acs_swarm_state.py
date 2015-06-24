ea1ed5b772defc1a457bb7f79e0848337190d0d4
Michael Day <mday299@yahoo.com>
2015-06-24 13:23:29 -0600

    swarm_state: Incoming UAV msgs are now the ACS_SwarmState responsibility

--- acs_lib/acs_swarm_state.py
+++ acs_lib/acs_swarm_state.py
@@ -9,6 +9,7 @@
 """
 
 from acs_lib import acs_uav_state
+from ap_lib import acs_messages
 
 class ACS_SwarmState(object):
     ''' 
@@ -21,7 +22,18 @@ class ACS_SwarmState(object):
         #Dictionary of UAVState classes that hold each UAV's state
         #This dictionary constitutes the Swarm.
         self.uav_states = {}
- 
+
+    def process_msg(self, msg):
+        if isinstance(msg, acs_messages.FlightStatus): 
+            self.update_uav_preprocess_msg(msg.msg_src, msg)
+            self.update_uav_state(msg.msg_src, msg)
+        elif isinstance(msg, acs_messages.Pose):
+            self.update_uav_preprocess_msg(msg.msg_src, msg)
+            self.update_uav_pose(msg.msg_src, msg)
+
+        #Currently not sending any info or throwing exceptions
+        #on unrecognized messages.
+
     def update_uav_preprocess_msg(self, id, msg):
         if id not in self.uav_states:
             self.uav_states[id] = acs_uav_state.ACS_UAVState(id)
@@ -29,10 +41,8 @@ class ACS_SwarmState(object):
         #TODO: process header
 
     def update_uav_state(self, id, msg):
-        self.update_uav_preprocess_msg(id, msg)
-    
-        #TODO: verify this is a FlightStatus message
-        
+        #print("%d %s %d %d" % (msg.msg_src, name, msg.armed, msg.mode))
+
         name = msg.name
 
         #TODO: remove this workaround when we switch everthing to Python3:
@@ -41,8 +51,6 @@ class ACS_SwarmState(object):
         self.uav_states[id].update_status(msg.msg_secs, name, msg.mode, msg.batt_rem, msg.batt_vcc, msg.ok_gps, msg.swarm_state, msg.msg_sub, msg.ctl_mode, msg.swarm_behavior)
 
     def update_uav_pose(self, id, msg):
-        self.update_uav_preprocess_msg(id, msg)
-
         quat = (msg.q_x, msg.q_y, msg.q_z, msg.q_w)
         self.uav_states[id].update_pose(msg.msg_secs, msg.lat, msg.lon, msg.alt, quat)
 
